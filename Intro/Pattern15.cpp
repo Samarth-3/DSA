11ac26b679c2abd8effa1bbd0b0aa75b75438897
Samarth-3 <spaliwal_be21@thapar.edu>
2023-06-14 10:39:02 +0530

    final commit

---
+++ Intro/Pattern15.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n=4;
+    for (int i = 4; i >0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+}
+
+// ****
+// ***
+// **
+// *
